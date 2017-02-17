7ef5dee63125d7ef3522f7323ed2ab32a3b26fe8
Rob Huehn <rhuehn@gmail.com>
2017-02-17 08:38:47 -0800

    thunk exec

--- src/network.js
+++ src/network.js
@@ -3,23 +3,13 @@
 
 import {exec} from 'child_process'
 
-const activeLink = new Promise(res => {
-    exec(`ip -o link |awk '$9=="UP"{print substr($2,1,length($2)-1)}'`, (error, stdout) => {
-        res(stdout.trim())
-    })
-})
+const
+    thunk = f => command => new Promise((r, j) => f(command, (error, stdout) => error ? j(error) : r(stdout.trim()))),
+    _exec = thunk(exec),
+    activeLink = _exec(`ip -o link |awk '$9=="UP"{print substr($2,1,length($2)-1)}'`)
 
-export const wake = mac => new Promise(async (res, rej) => {
-    exec(`etherwake ${mac} -i ${await activeLink}`, (error, stdout) => {
-        if (error) rej(error)
-        res(stdout.trim())
-    })
-})
-
-export const suspend = () => {
-    exec(`dbus-send --system --print-reply --dest=org.freedesktop.login1 /org/freedesktop/login1 org.freedesktop.login1.Manager.Suspend boolean:true`,
-        (error, stdout) => {
-            if (error) rej(error)
-            res(stdout.trim())
-        })
-}
+export const
+    wake = async mac =>
+        _exec(`etherwake ${mac} -i ${await activeLink}`),
+    suspend = () =>
+        _exec(`dbus-send --system --print-reply --dest=org.freedesktop.login1 /org/freedesktop/login1 org.freedesktop.login1.Manager.Suspend boolean:true`)
